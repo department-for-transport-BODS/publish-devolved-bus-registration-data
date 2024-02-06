8d209de01382a7ba70cd9c334b5168d6dcc41d96
SamerIssam <samer.salem@kpmg.co.uk>
2024-02-06 22:05:56 +0000

    csv vaidation error

--- backend/src/utils/csv_validator.py
+++ backend/src/utils/csv_validator.py
@@ -2,10 +2,11 @@ from fastapi import HTTPException
 from pydantic import BaseModel, Field, ValidationError
 from typing import List, Optional
 import csv
+from .logger import log
 class Registration(BaseModel):
     licenceNumber: str = Field(example="PC2021320")
     registrationNumber: str = Field(example="PD0001111/43010100")
-    routeNumber: int = Field(..., example="1")
+    routeNumber: int = Field(..., example=1)
     routeDescription: str = Field(..., example="Wigan - Highfield Grange Circular")
     variationNumber: int = Field(..., example=1)
     startPoint: str = Field(..., example="Wigan")
@@ -27,32 +28,43 @@ class Registration(BaseModel):
     otherDetails: Optional[str] = Field(None, example="")
 
 
+def csv_data_structure_check(csv_data: [dict]) -> dict:
+    '''This function checks the structure of the CSV data structure and returns a dictionary of valid and invalid records.
+    It checs if a records adheres to the structure of the Registration model.
+    Functionalities:
+    1. Validate each record and deserialize it into a Python object.
+    2. Invalide records are stored in a validation_errors list along with the record number.
+    3. Valid records are stored in a pydantic_models list.
+    4. Returns a dictionary of valid and invalid records.
 
-# first_name,last_name,addresss
-class person(BaseModel):
-    first_name: str = Field(..., min_length=1, max_length=5)
-    last_name: str = Field(..., min_length=1, max_length=20)
-    address: str
-    
-
-def validate_csv(csv_data: [dict]):
+    '''
     # Convert the CSV data into a list of dictionaries
     # csv_data = list(csv.DictReader(file))
     pydantic_models = []
     validation_errors = []
     
-    for data_dict in csv_data:
+    for idx, data_dict in enumerate(csv_data):
         try:
             # Validate each record and deserialize it into a Python object.
             pydantic_model = Registration(**data_dict)
             pydantic_models.append(pydantic_model)
         except ValidationError as e:
-            validation_errors.append(e.errors())
-    if pydantic_models:
-        print(len(pydantic_models))
-        # send_registration_records_to_db(pydantic_models)
-    if validation_errors:
-        print(validation_errors)
-        raise HTTPException(status_code=400, detail=[{"Invalid_records":len(validation_errors)},validation_errors])
-    print(pydantic_models)
-    return {"Message": "Data is valid."}
+            errors = e.errors()
+            validation_errors.append({"recrod_number": idx + 1,"errors": errors})
+    return {"Invalid_records":validation_errors,"Valid_records":pydantic_models}
+
+
+def delete_url_field_and_rename_loc_to_field(validation_errors: [dict]) -> [dict]:
+    '''This function has 2 functionalities.
+    1. It removes the "url" field from the validation errors.
+    2. It renames the "loc" key to "field".
+    ''' 
+    edited_errors = []
+    from pprint import pprint
+    for record in validation_errors:
+        for error in record["errors"]:
+            error["field"] = error.pop("loc")
+            del error["url"]
+        edited_errors.append(record)
+    pprint(edited_errors,indent=4) 
+    return edited_errors
