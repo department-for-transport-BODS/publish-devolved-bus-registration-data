7ea99f5fd6d0b7f1cff4706675e2563f4a87cd2e
SamerIssam <samer.salem@kpmg.co.uk>
2024-02-09 12:49:31 +0000

    add config file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,32 +1,21 @@
 import csv
-import os
 from io import StringIO
 
-from dotenv import load_dotenv
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
+from .utils.config import (ALLOW_HEADER, ALLOW_METHODS, ALLOW_ORIGINS,
+                           AWS_REGION, ENVIRONMENT)
 from .utils.csv_validator import csv_data_structure_check
-from .utils.logger import log
-
-load_dotenv()
-log.info(f"Running in regions: {os.getenv('AWS_REGION', 'Running locally')}")
-log.info(f"Running in environment: {os.getenv('DB_HOST', 'Running locally')}")
-
 
 app = FastAPI()
-
-origins = [
-    '*'
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=ALLOW_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=ALLOW_METHODS,
+    allow_headers=ALLOW_HEADER,
 )
 
 
@@ -34,9 +23,9 @@ app.add_middleware(
 def read_root():
     return {
         "message": "FastAPI running on AWS Lambda and is executed in region "
-        + os.getenv("AWS_REGION", "Running locally")
+        + AWS_REGION
         + ", using runtime environment "
-        + os.getenv("AWS_EXECUTION_ENV", "Running locally")
+        + ENVIRONMENT
     }
 
 
