be42e8623356a22870b05271e86005bda7e7fccd
Samer Salem <109161272+SamerIssam@users.noreply.github.com>
2024-03-04 09:34:07 +0000

    Feature/bods 2591 create login page for UI (#15)
    * csv connection error
    
    * Adding upload partly
    
    * pages templates
    
    * Adding upload Confirmation
    
    * upload confirmation
    
    * record to db
    
    * send to db using classses
    
    * using live data
    
    * test db
    
    * remove extra function
    
    * delete unused packagesa
    
    * add logger
    
    * Add comments
    
    * change operator id column
    
    * add amplify
    
    * add aws creds
    
    * linting
    
    * Adding login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
 import "./App.css";
 import "./Sass/App.scss";
 import Home from "./pages/home";
-import FormExample from "./pages/formexample";
 import UploadCSVPage from "./pages/uploadcsv";
 import Basic from "./pages/basic";
 import DefaultPage from "./pages/default";
-import Form from "./pages/Form";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PartlyUploading from "./pages/uploadConfirmation";
 import SuccessfullyUPloaded from "./pages/uploadConfirmationSuccessfully";
 import LoginPage from "./pages/login";
-
+import { Amplify } from "aws-amplify";
+import AmplifyConfiguration from "./utils/login/AmplifyConfig";
+import RequireAuth from "./utils/login/requireAuth";
 function App() {
+  Amplify.configure(AmplifyConfiguration);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/example" element={<FormExample />} />
-        <Route path="/uploadcsv" element={<UploadCSVPage />} />
+        <Route
+          path="/uploadcsv"
+          element={<RequireAuth component={UploadCSVPage} />}
+        />
         <Route path="/basic" element={<Basic />} />
         <Route path="/default" element={<DefaultPage />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/partlyuploaded" element={<PartlyUploading />} />
+        <Route
+          path="/partlyuploaded"
+          element={<RequireAuth component={PartlyUploading} />}
+        />
         <Route
           path="/successfullyuploaded"
-          element={<SuccessfullyUPloaded />}
+          element={<RequireAuth component={SuccessfullyUPloaded} />}
         />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
---
+++ frontend/src/utils/login/requireAuth.tsx
@@ -0,0 +1,52 @@
+import React from "react"; // Import the 'React' module
+
+import { getCurrentUser } from "aws-amplify/auth";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function RequireAuth({
+  component: Component,
+  ...rest
+}: {
+  component: React.ComponentType<any>;
+}) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [state, setState] = useState("loading");
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    // Fetch user data from an API or perform any necessary logic here
+    // For example:
+    const fetchUserData = async () => {
+      try {
+        const user = await getCurrentUser();
+        if (user) {
+          setIsLoggedIn(true);
+          setState("authenticated");
+        }
+      } catch (error) {
+        // get current page
+        console.log("location: ", location);
+        // remove("/" from the location.pathname)
+        const currentPath = location.pathname.substring(1);
+        // window.location.replace("/login");
+        navigate(`/login?to=${currentPath}`, {
+          state: { error: "You are not logged in" },
+        });
+        console.error("Error fetching user data: ", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  /* If in loading state, return loading message while waiting for 
+        isValidToken to complete */
+  if (state === "loading") {
+    return null;
+  }
+
+  return <Component isLoggedIn={isLoggedIn} {...rest} />;
+}
+
+export default RequireAuth;
