8cd135d0a3130ef4919f5e8bd1415ae045d95ac2
Samer Salem <109161272+SamerIssam@users.noreply.github.com>
2024-03-12 10:14:31 +0000

    jwt automatically picked up (#18)

--- frontend/src/utils/SendCSV.tsx
+++ frontend/src/utils/SendCSV.tsx
@@ -5,18 +5,27 @@ export const SendCSV = async (formData: FormData, navigate:any) => {
     const apiBaseUrl = getApiUrl();
 
     try {
+        let jwt = "";
+        Object.entries(localStorage).forEach(([key, value]) => {
+            if (key.includes("accessToken")) {
+
+                jwt = value.toString();
+            }
+        });
         const response = await axios.post(
-            `${apiBaseUrl}/api/uploadfile`,
+            `${apiBaseUrl}/api/v1/uploadfile`,
             formData,
             {
                 headers: {
                     "Access-Control-Allow-Origin": "*",
                     "Content-Type": "multipart/form-data",
+                    "Authorization": "Bearer " + jwt,
                 },
             }
         );
         navigate("/successfullyuploaded", { state: response?.data, replace: true });
     } catch (error) {
+        
         const nowData = {
             message: (error as AxiosError).message,
             code: (error as AxiosError).code,
