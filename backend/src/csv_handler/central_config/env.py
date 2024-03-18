7deafeac3d9747f91334f322cf86ff7a8b163bc4
Samer Salem <109161272+SamerIssam@users.noreply.github.com>
2024-03-18 15:51:52 +0000

    Update backend/src/csv_handler/central_config/env.py
    Co-authored-by: Rob Steel <142216849+robsteel-kpmg@users.noreply.github.com>

--- backend/src/csv_handler/central_config/env.py
+++ backend/src/csv_handler/central_config/env.py
@@ -10,6 +10,6 @@ LOGGER_LEVEL = getenv("LOG_LEVEL", "INFO")
 LOGGER_MOD = getenv("LOGGER_MOD", "local")
 # AWS env
 AWS_REGION = getenv("AWS_REGION", "Running locally")
-USERPOOL_ID = getenv("COGNITO_USERPOOL_ID", "USERPOOL_ID is not set")
+USERPOOL_ID = getenv("COGNITO_USERPOOL_ID", "COGNITO_USERPOOL_ID is not set")
 APP_CLIENT_ID = getenv("COGNITO_APP_CLIENT_ID", "APP_CLIENT_ID is not set")
 OTC_CLIENT_API_URL = getenv("OTC_CLIENT_API_URL", "OTC_API_URL is not set")
