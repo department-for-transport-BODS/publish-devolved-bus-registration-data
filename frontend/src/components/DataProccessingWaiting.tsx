26b05b8bcd6f4fd95721e3cf59d740d4f3906673
Samer Salem <109161272+SamerIssam@users.noreply.github.com>
2024-03-18 15:57:13 +0000

    Update frontend/src/components/DataProccessingWaiting.tsx
    Co-authored-by: Rob Steel <142216849+robsteel-kpmg@users.noreply.github.com>

--- frontend/src/components/DataProccessingWaiting.tsx
+++ frontend/src/components/DataProccessingWaiting.tsx
@@ -9,7 +9,7 @@ const DataProccessingWaiting: React.FC = () => {
         <h3 className="govuk-panel__title">Your data is being processed</h3>
         <div className="govuk-panel__body ">
           <div className="govuk-!-font-size-19">
-            Once successfully validated the data set details will be shown.
+            Once successfully validated the dataset details will be shown.
           </div>
           <div className="govuk-!-display-inline-block">
             <BarLoader color={"#ffffff"} loading={true} width={300} height={6} />
