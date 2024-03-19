cd5aed7bb5677a6e0deff3c285c4e1ca96233f7c
Rob Steel <=>
2024-03-19 11:27:59 +0000

    Correct sequence

--- sql/e.registrationArchive.sql
+++ sql/e.registrationArchive.sql
@@ -1,8 +1,28 @@
-CREATE OR REPLACE TRIGGER check_duplicate_records
-AFTER UPDATE ON ep_registration
-FOR EACH ROW
-EXECUTE FUNCTION handle_duplicate_records();
-
+CREATE TABLE IF NOT EXISTS registration_archive (
+    id SERIAL PRIMARY KEY,
+    otc_licence_id INTEGER,
+    route_number VARCHAR(255),
+    route_description VARCHAR(255),
+    variation_number INTEGER,
+    start_point VARCHAR(255),
+    finish_point VARCHAR(255),
+    via VARCHAR(255),
+    subsidised VARCHAR(10),
+    subsidy_detail VARCHAR(255),
+    is_short_notice BOOLEAN,
+    received_date DATE,
+    granted_date DATE,
+    effective_date DATE,
+    end_date DATE,
+    otc_operator_id INTEGER,
+    bus_service_type_id VARCHAR(255),
+    bus_service_type_description VARCHAR(255),
+    registration_number VARCHAR(255),
+    traffic_area_id VARCHAR(255),
+    application_type VARCHAR(255),
+    publication_text VARCHAR(255),
+    other_details VARCHAR(255));
+    
 CREATE OR REPLACE FUNCTION handle_duplicate_records()
 RETURNS TRIGGER AS
 $func$
@@ -54,27 +74,7 @@ END;
 $func$
 LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS registration_archive (
-    id SERIAL PRIMARY KEY,
-    otc_licence_id INTEGER,
-    route_number VARCHAR(255),
-    route_description VARCHAR(255),
-    variation_number INTEGER,
-    start_point VARCHAR(255),
-    finish_point VARCHAR(255),
-    via VARCHAR(255),
-    subsidised VARCHAR(10),
-    subsidy_detail VARCHAR(255),
-    is_short_notice BOOLEAN,
-    received_date DATE,
-    granted_date DATE,
-    effective_date DATE,
-    end_date DATE,
-    otc_operator_id INTEGER,
-    bus_service_type_id VARCHAR(255),
-    bus_service_type_description VARCHAR(255),
-    registration_number VARCHAR(255),
-    traffic_area_id VARCHAR(255),
-    application_type VARCHAR(255),
-    publication_text VARCHAR(255),
-    other_details VARCHAR(255));
+CREATE OR REPLACE TRIGGER check_duplicate_records
+AFTER UPDATE ON ep_registration
+FOR EACH ROW
+EXECUTE FUNCTION handle_duplicate_records();
