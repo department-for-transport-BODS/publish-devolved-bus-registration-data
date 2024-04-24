fd261a4de587c27b868e154dd7b0011f4a87d484
SamerIssam <samer.salem@kpmg.co.uk>
2024-04-24 10:38:42 +0100

    BODS-4531

--- frontend/src/pages/accessibilityStatement.tsx
+++ frontend/src/pages/accessibilityStatement.tsx
@@ -23,10 +23,10 @@ const AccessibilityStatement: React.FC = () => {
             be clear and simple to understand. You should be able to:
           </p>
           <ul>
-            <li>Zoom in up to 300% without problems.</li>
+            <li>Zoom in up to 300% without problems</li>
             <li>
               Navigate all of the website using just a keyboard (except for maps
-              which are not essential to the functionality).
+              which are not essential to the functionality)
             </li>
             <li>
               Navigate all of the website using speech recognition software.
