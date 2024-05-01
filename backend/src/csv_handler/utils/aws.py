60bccf9e6504f065ea5eaa82555dfd40da654fa8
SamerIssam <samer.salem@kpmg.co.uk>
2024-05-01 20:22:50 +0100

    use class

--- backend/src/csv_handler/utils/aws.py
+++ backend/src/csv_handler/utils/aws.py
@@ -1,9 +1,9 @@
+from os import getenv
 import boto3
 from botocore.exceptions import ClientError
-# from central_config import AWS_REGION
-# from .logger import log, console
+from central_config import AWS_REGION, BUCKET_NAME, PROJECT_ENV
+from .logger import log
 from time import sleep
-
 def get_secret(secret_name: str):
     session = boto3.session.Session()
     client = session.client(
@@ -58,80 +58,100 @@ def upload_file_to_S3(
         except Exception as e:
             log.error(f'Errors: {e}')
 
-def upload_bstring_to_S3_as_file(
-           bucket_name, binary_data, file_name
-        ) -> None:
-    try:
-        boto3.setup_default_session(profile_name='AWSPowerUserAccess-992382470170')
-        # Initialize the S3 client
-        client = boto3.client(
-            service_name='s3',
-            )
-
-        # Specify the bucket name and the key (including another filename)
-        object_key = f'dev/{file_name}.csv'
-
-        # # Upload the binary data
-        client.put_object(Body=binary_data, Bucket=bucket_name, Key=object_key)
-    except Exception as e:
-        print(f'Errors: {e}')
-        raise Exception(f'Errors: Could not upload file to S3.')
 
 
-def read_file_tags(bucket_name, file_name):
-    try:
-        # Initialize the S3 client
-        boto3.setup_default_session(profile_name='AWSPowerUserAccess-992382470170')
-        client = boto3.client('s3')
-
-        # Get the object tagging
-        object_key = f'dev/{file_name}.csv'
-        response = client.get_object_tagging(Bucket=bucket_name, Key=object_key)
-
-        # 'TagSet': [{'Key': 'scanned-by', 'Value': 'ClamAV'}, {'Key': 'av-status', 'Value': 'clean'}]
-        # Return the tags
-        tag_set = response.get('TagSet')
-        if tag_set and len(tag_set) > 0:
-            return tag_set
-        
-    except Exception as e:
-        print(f'Errors: {e}')
-        raise Exception('Errors: Could not read file tags from S3.')
-    
-
-def delete_file_from_s3(bucket_name, file_name):
-    try:
-        # Initialize the S3 client
-        boto3.setup_default_session(profile_name='AWSPowerUserAccess-992382470170')
-        client = boto3.client('s3')
-
-        # Delete the object
-        object_key = f'dev/{file_name}.csv'
-        client.delete_object(Bucket=bucket_name, Key=object_key)
-    except Exception as e:
-        print(f'Errors: {e}')
-        raise Exception('Errors: Could not delete file from S3.')
+class ClamAVClient:
+    def __init__(self, file_name, data):
+        if not BUCKET_NAME:
+            raise Exception("Bucket name is not set.")
+        self.bucket_name = BUCKET_NAME
+        self.s3_folder = PROJECT_ENV if PROJECT_ENV != 'local' else 'dev'
+        self.file_name = file_name
+        self.data = data
+        self.scan_file(self.bucket_name, self.s3_folder, self.file_name, self.data)
 
 
-if __name__ == '__main__':
-    file_name = 'test5'
-    bucket_name = 'shared-pdbrd-clamav-artefacts-992382470170'
-    data = b'test data'
-    def check_file(bucket_name,file_name, data):
-        upload_bstring_to_S3_as_file(bucket_name, data, file_name)
+    async def scan_file(self, bucket_name,s3_folder, file_name, data):
+        self.upload_bstring_to_S3_as_file(bucket_name,s3_folder, file_name, data)
         print(f"File {file_name} is uploaded to S3 bucket {bucket_name}.")
         for i in range(1,11):
             sleep(10)
-            res = read_file_tags(bucket_name, file_name)
+            res = self.read_file_tags(bucket_name,s3_folder, file_name)
             if res is not None:
-                print(f"Getting tags is done, after {i} attempts.")
-                print(res)
-                delete_file_from_s3(bucket_name, file_name)
-                print(f"File {file_name} is deleted from S3 bucket {bucket_name}.")
+                log.info(f"Getting tags is done, after {i} attempts.")
+                self.delete_file_from_s3(bucket_name,s3_folder, file_name)
+                log.info(f"File {file_name} is deleted from S3 bucket.")
                 break
             print(f"Attempt {i} to get tags is not successful.")
-
-
         av_status = [item['Value'] for item in res if item['Key'] == 'av-status']
+
         if len(av_status) > 0:
-            print(f"File is {av_status[0]}.")
+            if av_status[0] == 'clean':
+                return 
+                
+        
+        raise Exception("File did not pass antivirus check.")
+
+
+    def get_boto_client(self):
+        boto3.setup_default_session(profile_name='AWSPowerUserAccess-992382470170')
+        return boto3.client(service_name='s3',region_name=AWS_REGION,)
+
+        
+
+
+    def upload_bstring_to_S3_as_file(
+            self,bucket_name, s3_folder,file_name, binary_data
+            ) -> None:
+        try:
+            client = self.get_boto_client()
+            
+            # Specify the bucket name and the key (including another filename)
+            object_key = f'{s3_folder}/{file_name}.csv'
+
+            # # Upload the binary data
+            client.put_object(Body=binary_data, Bucket=bucket_name, Key=object_key)
+        except Exception as e:
+            print(f'Errors: {e}')
+            raise Exception(f'Errors: Could not upload file to S3.')
+
+
+    def read_file_tags(self,bucket_name,s3_folder, file_name):
+        try:
+            # Initialize the S3 client
+            client = self.get_boto_client()
+            # Get the object tagging
+            object_key = f'{s3_folder}/{file_name}.csv'
+            print(f"Reading tags for file {object_key}.")
+            response = client.get_object_tagging(Bucket=bucket_name, Key=object_key)
+
+            # 'TagSet': [{'Key': 'scanned-by', 'Value': 'ClamAV'}, {'Key': 'av-status', 'Value': 'clean'}]
+            # Return the tags
+            if response is not None:
+                tag_set = response.get('TagSet')
+                if tag_set and len(tag_set) > 0:
+                    return tag_set
+            
+        except Exception as e:
+            print(f'Errors: {e}')
+            raise Exception('Errors: Could not read file tags from S3.')
+        
+
+    def delete_file_from_s3(self, bucket_name,s3_folder, file_name):
+        try:
+            # Initialize the S3 client
+            client = self.get_boto_client()
+            # Delete the object
+            object_key = f'{s3_folder}/{file_name}.csv'
+            client.delete_object(Bucket=bucket_name, Key=object_key)
+        except Exception as e:
+            print(f'Errors: {e}')
+            raise Exception('Errors: Could not delete file from S3.')
+
+
+# if __name__ == '__main__':
+    # print(os.getcwd())
+    # file_name = 'test5'
+    # bucket_name = 'shared-pdbrd-clamav-artefacts-992382470170'
+    # data = b'test data'
+    # ClamAVClient(file_name, data)
