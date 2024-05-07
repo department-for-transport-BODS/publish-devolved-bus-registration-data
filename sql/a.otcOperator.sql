91454adaeedea750d9eeaec114e332f86bcbfcc8
SamerIssam <samer.salem@kpmg.co.uk>
2024-05-07 20:49:51 +0100

    update db

--- sql/a.otcOperator.sql
+++ sql/a.otcOperator.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS otc_operator (
     id SERIAL PRIMARY KEY,
-    operator_name VARCHAR(255) UNIQUE,
+    operator_name VARCHAR(255) UNIQUE
 );
--- sql/b.otcLicence.sql
+++ sql/b.otcLicence.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS otc_licence (
     id SERIAL PRIMARY KEY,
     licence_number VARCHAR(255) UNIQUE,
-    licence_status VARCHAR(255),
+    licence_status VARCHAR(255)
 );
