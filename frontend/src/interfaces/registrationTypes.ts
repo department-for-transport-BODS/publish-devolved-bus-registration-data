d4a3eada3a1fc654c4a6f6849918b3e389ac7e01
Deanna <deanna.sharma@kainos.com>
2026-07-22 14:00:06 +0100

    fix: fixes for frontend linting errors

---
+++ frontend/src/interfaces/registrationTypes.ts
@@ -0,0 +1,26 @@
+export interface Registration {
+  registrationNumber?: string;
+  licenceNumber?: string;
+  routeNumber?: string;
+  description?: string;
+  variationNumber?: number;
+  startPoint?: string;
+  finishPoint?: string;
+  via?: string;
+  subsidised?: string;
+  subsidyDetail?: string;
+  isShortNotice?: boolean;
+  receivedDate?: string;
+  grantedDate?: string;
+  effectiveDate?: string;
+  endDate?: string;
+  operatorName?: string;
+  busServiceTypeId?: string;
+  busServiceTypeDescription?: string;
+  trafficAreaId?: string;
+  applicationType?: string;
+  publicationText?: string;
+  otherDetails?: string;
+  licenceStatus?: string;
+}
+
